e900dbf81f202a15640db1684dbb448372f8a68c
Matthew Manning <mmanning88@gmail.com>
2020-05-30 16:37:48 -0600

    Initial Commit

---
+++ src/Controller/LoginScreenController.java
@@ -0,0 +1,32 @@
+
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+public class LoginScreenController implements Initializable {
+    
+    @FXML
+    private Label loginInstructionLbl;
+
+    @FXML
+    void onActionExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionLogin(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }    
+    
+}
---
+++ src/Controller/MainScreenController.java
@@ -0,0 +1,71 @@
+
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+public class MainScreenController implements Initializable {
+    
+     @FXML
+    private TableView<?> mainScreenCalanderView;
+
+    @FXML
+    void OnActionDeleteAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionModifyAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionToReports(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAddAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAddCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionDeleteCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionModifyCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionMonthView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionWeekView(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }    
+    
+}
